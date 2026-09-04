5a31c1b9daa083e2bb532ab3dc4d86204d1ce4d0
ViniciusSouzaDosReis <vinireis010305@gmail.com>
2026-09-04 15:39:46 -0300

    add env for remote settings page

--- apps/investments/vite.config.ts
+++ apps/investments/vite.config.ts
@@ -1,25 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { federation } from '@module-federation/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), federation({
-    name: 'vini_investments',
-    filename: 'remoteEntry.js',
-    shared: {
-      react: {
-        singleton: true,
-        requiredVersion: '^19.0.0',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+
+  return {
+    plugins: [react(), tailwindcss(), federation({
+      name: 'vini_investments',
+      filename: 'remoteEntry.js',
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: '^19.0.0',
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: '^19.0.0',
+        },
       },
-      'react-dom': {
-        singleton: true,
-        requiredVersion: '^19.0.0',
+      remotes: {
+        settings: `settings@${env.VITE_REMOTE_SETTINGS}`
       },
-    },
-    remotes: {
-      settings: 'settings@http://localhost:5174/mf-manifest.json'
-    },
-  })],
+    })],
+  }
 })
